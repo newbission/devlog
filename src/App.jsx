69dc3857fbd314f5e9dee63bab34dceef40205f2
newbission <newbission@gmail.com>
2024-02-01 16:17:58 +0900

    Init: vite-react 생성 및 index.html 수정

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h1>블로그 준비중</h1>
+      <a href="https://github.com/newbission"><h3>newbission</h3></a>
+    </>
+  )
+}
+
+export default App
